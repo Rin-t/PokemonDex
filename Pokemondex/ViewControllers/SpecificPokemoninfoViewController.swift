0711cc841c6c530e664b00ae6fa2361f800119b7
Rin <rin.t.4921@gmail.com>
2021-09-20 20:25:57 +0900

    色違いと通常色を切り替えるボタンを配置

--- Pokemondex/ViewControllers/SpecificPokemoninfoViewController.swift
+++ Pokemondex/ViewControllers/SpecificPokemoninfoViewController.swift
@@ -33,11 +33,13 @@ final class SpecificPokemoninfoViewController: UIViewController {
         view.addSubview(underLabelShadowView)
         view.addSubview(monsterBallImage)
         view.addSubview(pokemonView)
+        view.addSubview(bottomControlView)
 
         nameLabel.anchor(top: view.topAnchor, centerX: view.centerXAnchor ,width: UIScreen.main.bounds.width * 0.5, height: 35, topPadding: UIScreen.main.bounds.height * 0.15)
         underLabelShadowView.anchor(top: view.topAnchor, centerX: view.centerXAnchor ,width: UIScreen.main.bounds.width * 0.5, height: 35, topPadding: UIScreen.main.bounds.height * 0.15)
         monsterBallImage.anchor(centerY: nameLabel.centerYAnchor, centerX: nameLabel.leftAnchor, width: 50, height: 50)
         pokemonView.anchor(top: nameLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, height: 500, topPadding: 40, leftPadding: 20, rightPadding: 20)
+        bottomControlView.anchor(top: pokemonView.bottomAnchor, bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, topPadding: 20)
 
     }
 }
--- Pokemondex/Views/SpecificPokemonInfo/BottomButtonsView.swift
+++ Pokemondex/Views/SpecificPokemonInfo/BottomButtonsView.swift
@@ -7,6 +7,85 @@
 
 import UIKit
 
+import UIKit
+
 final class BottomButtonsView: UIView {
-    
+
+    let nomalColorButton = BottomButtonView(frame: .zero, width: 100, buttonTitle: "nomal")
+    let shinyColorButton = BottomButtonView(frame: .zero, width: 100, buttonTitle: "shiny")
+
+    init() {
+        super.init(frame: .zero)
+        let basestackView = UIStackView(arrangedSubviews: [nomalColorButton, shinyColorButton])
+        basestackView.axis = .horizontal
+        basestackView.distribution = .fillEqually
+        basestackView.spacing = 10
+        basestackView.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(basestackView)
+        basestackView.anchor(top: topAnchor, bottom: bottomAnchor, left: leftAnchor, right: rightAnchor, leftPadding: 10, rightPadding: 10)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init has been implemented")
+    }
+
 }
+
+final class BottomButtonView: UIView {
+
+    var button: BottomButton?
+    private let screenHeight = UIScreen.main.bounds.height
+    private let screenWidth = UIScreen.main.bounds.width
+
+    init(frame: CGRect, width: CGFloat, buttonTitle: String) {
+        super.init(frame: frame)
+        button = BottomButton(type: .custom)
+        button?.translatesAutoresizingMaskIntoConstraints = false
+        button?.backgroundColor = .white
+        button?.layer.cornerRadius = 10
+        button?.setTitle(buttonTitle, for: .normal)
+        button?.setTitleColor(.black, for: .normal)
+        button?.layer.shadowOffset = .init(width: 1.5, height: 2)
+        button?.layer.shadowColor = UIColor.black.cgColor
+        button?.layer.shadowOpacity = 0.3
+        button?.layer.shadowRadius = 15
+
+        guard let button = button else { return }
+        addSubview(button)
+
+        button.anchor(centerY: centerYAnchor, centerX: centerXAnchor, width: screenWidth * 0.3, height: screenHeight * 0.08)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class BottomButton: UIButton {
+
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.7, options: []) {
+                    self.transform = .init(scaleX: 0.8, y: 0.8)
+                    self.layoutIfNeeded()
+                }
+            } else {
+                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.7, options: []) {
+                    self.transform = .identity
+                    self.layoutIfNeeded()
+                }
+            }
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
